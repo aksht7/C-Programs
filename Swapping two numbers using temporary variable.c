6458d1e125ba81970b0ac0cba89aa1c1adf20526
Akshat Gupta <37541022+aksht7@users.noreply.github.com>
2018-06-22 21:47:57 +0530

    Update Swapping two numbers using temporary variable.c

--- Swapping two numbers using temporary variable.c
+++ Swapping two numbers using temporary variable.c
@@ -14,9 +14,9 @@ int main()                      /*Main Driving Program*/
     printf("The Entered numbers are %d and %d\n",num1,num2);
 
     temp=num1;                          /*Swapping Operation/Algorithm*/
-
-    num1=num2;
-
+                           
+    num1=num2;                    //temp variable is used to store the value of num1. Therefore value of num2 is stored at the address of num1.
+                                  //Then value of num1 is stored at address of num2 which is present in temp. This cause swapping.
     num2=temp;
 
     printf("Numbers after swapping are %d and %d\n",num1,num2);
