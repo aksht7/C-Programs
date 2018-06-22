0d70bd3182ae79f41e2cda6e16ccad21ef70c988
Himanshu Verkiya <34170205+Verkiya@users.noreply.github.com>
2018-06-22 21:28:19 +0530

    Update Program to reverse a number.c

--- Program to reverse a number.c
+++ Program to reverse a number.c
@@ -1,17 +1,17 @@
-               //This program allows you to reverse a number
-#include <stdio.h> //for standard input output
+                        //This program allows you to reverse a number
+#include <stdio.h>      //for standard input output
 int main()
 {
   long num, rem, rev=0;
   printf("Enter a Number : ");
   scanf("%ld",&num); 
-  //algorithm to reverse a number
+                        //algorithm to reverse a number
   while(num)
   {
   	rem=num%10;
   	rev=rev*10+rem;
-  	num/=10;   //it is equal to num=num/10
+  	num/=10;            //it is equal to num=num/10
   }
   printf("Reversed number is : %ld",rev);
-  return 0;     //Happy Coding :)
+  return 0;             //Happy Coding :)
 }
