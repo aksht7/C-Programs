3daff33f1c8de133ab7d322bea050b0d8866d623
Himanshu Verkiya <34170205+Verkiya@users.noreply.github.com>
2018-06-22 19:18:13 +0530

    Update Swapping two numbers using temporary variable.c

--- Swapping two numbers using temporary variable.c
+++ Swapping two numbers using temporary variable.c
@@ -4,22 +4,23 @@
 
 int main()                      /*Main Driving Program*/
 {
+  
     int num1,num2,temp;
 
-printf("Enter Two Numbers\n");
+    printf("Enter Two Numbers\n");
 
-scanf("%d %d",&num1,&num2);     /*Entered two numbers*/
+    scanf("%d %d",&num1,&num2);     /*Entered two numbers*/
 
-printf("The Entered numbers are %d and %d\n",num1,num2);
+    printf("The Entered numbers are %d and %d\n",num1,num2);
 
-temp=num1;                          /*Swapping Operation/Algorithm*/
+    temp=num1;                          /*Swapping Operation/Algorithm*/
 
-num1=num2;
+    num1=num2;
 
-num2=temp;
+    num2=temp;
 
-printf("Numbers after swapping are %d and %d\n",num1,num2);
+    printf("Numbers after swapping are %d and %d\n",num1,num2);
 
-return 0;
+    return 0;
 
 }
